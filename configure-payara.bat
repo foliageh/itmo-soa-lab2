5547d8a1aedd6360efb361690a00553eb55968df
foliageh <mygazztop1@gmail.com>
2025-10-11 14:30:23 +0300

    add https

--- configure-payara.bat
+++ configure-payara.bat
@@ -31,5 +31,21 @@ REM Создаем JDBC ресурс
 "%ASADMIN%" create-jdbc-resource --enabled=true --poolName=postgres-pool --target=domain datasources/PostgresDS
 "%ASADMIN%" create-resource-ref --enabled=true --target=server datasources/PostgresDS
 
+REM Генерируем сертификат (везде пароль ставить password)
+REM cd C:\payara7\glassfish\domains\domain1\config
+REM $env:OPENSSL_CONF="C:\Program Files\Git\usr\ssl\openssl.cnf"
+REM openssl req -x509 -newkey rsa:4096 -keyout mycert.key -out mycert.crt -days 365 -subj "/C=RU/ST=SPB/L=SPB/O=ITMO/CN=localhost" -addext "subjectAltName=DNS:localhost,IP:127.0.0.1"
+REM openssl pkcs12 -export -in mycert.crt -inkey mycert.key -out mycert.p12 -name mydomain_certificate -passout pass:password
+REM *остановить payara*
+REM "C:\payara7\bin\asadmin.bat" change-master-password --savemasterpassword=true domain1
+REM REM Enter the current master password>changeit
+REM REM Enter the new master password>password
+REM keytool -importkeystore -destkeystore keystore.p12 -srckeystore mycert.p12 -srcstoretype PKCS12 -alias mydomain_certificate -deststorepass password -srcstorepass password
+REM если mydomain_certificate уже существует: keytool -delete -alias mydomain_certificate -keystore cacerts.p12 -storepass password
+REM keytool -importcert -trustcacerts -destkeystore cacerts.p12 -file mycert.crt -alias mydomain_certificate -storepass password
+REM REM Trust this certificate? [no]:  yes
+REM *запустить payara*
+REM "C:\payara7\bin\asadmin.bat" set configs.config.server-config.network-config.protocols.protocol.http-listener-2.ssl.cert-nickname=mydomain_certificate
+
 echo === Конфигурация завершена ===
 pause
